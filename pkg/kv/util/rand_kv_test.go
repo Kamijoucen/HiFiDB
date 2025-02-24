976277206c5f98a9f28a1a661ad8aa2a6b62423a
kamijoucen <kamijoucen@foxmail.com>
2025-02-25 00:08:12 +0800

    单元测试

---
+++ pkg/kv/util/rand_kv_test.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetTestKey(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		assert.NotNil(t, string(GetTestKey(i)))
+	}
+}
+
+func TestRandomValue(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		assert.NotNil(t, string(RandomValue(10)))
+	}
+}
