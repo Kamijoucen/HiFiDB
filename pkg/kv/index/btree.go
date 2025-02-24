96aa8908b50888379f5cf7b6fdc07838aa2f9c08
kamijoucen <kamijoucen@foxmail.com>
2025-02-25 01:16:25 +0800

    索引查询

--- pkg/kv/index/btree.go
+++ pkg/kv/index/btree.go
@@ -53,3 +53,53 @@ func (b *BTreeIndex) Delete(key []byte) bool {
 	b.lock.Unlock()
 	return oldItem != nil
 }
+
+// BTree 索引迭代器
+type btreeIterator struct {
+	curIndex int     // 当前遍历的下表位置
+	reverse  bool    // 是否反向遍历
+	values   []*Item // 遍历的数据
+}
+
+func newBTreeIterator(tree *btree.BTree, reverse bool) *btreeIterator {
+	return &btreeIterator{
+		curIndex: 0,
+		reverse:  reverse,
+		values:   make([]*Item, tree.Len()),
+	}
+}
+
+// Rewind 回到起始位置
+func (i *btreeIterator) Rewind() {
+
+}
+
+// Seek 移动第一个大于等于key的位置
+func (i *btreeIterator) Seek(key []byte) {
+	panic("not implemented") // TODO: Implement
+}
+
+// Next 移动到下一个key
+func (i *btreeIterator) Next() {
+	panic("not implemented") // TODO: Implement
+}
+
+// Valid 是否有效，即是否还有下一个key，用于退出循环
+func (i *btreeIterator) Valid() (_ bool) {
+	panic("not implemented") // TODO: Implement
+}
+
+// Key 返回当前位置key
+func (i *btreeIterator) Key() (_ []byte) {
+	panic("not implemented") // TODO: Implement
+}
+
+// Value 返回当前位置value
+func (i *btreeIterator) Value() (_ *data.LogRecordPos) {
+	panic("not implemented") // TODO: Implement
+}
+
+// Close 关闭迭代器
+func (i *btreeIterator) Close() {
+	panic("not implemented") // TODO: Implement
+}
--- pkg/kv/index/index.go
+++ pkg/kv/index/index.go
@@ -34,3 +34,28 @@ type Item struct {
 func (i *Item) Less(bi btree.Item) bool {
 	return bytes.Compare(i.key, bi.(*Item).key) < 0
 }
+
+// Iterator 索引迭代器
+type Iterator interface {
+
+	// Rewind 回到起始位置
+	Rewind()
+
+	// Seek 移动第一个大于等于key的位置
+	Seek(key []byte)
+
+	// Next 移动到下一个key
+	Next()
+
+	// Valid 是否有效，即是否还有下一个key，用于退出循环
+	Valid() bool
+
+	// Key 返回当前位置key
+	Key() []byte
+
+	// Value 返回当前位置value
+	Value() *data.LogRecordPos
+
+	// Close 关闭迭代器
+	Close()
+}
